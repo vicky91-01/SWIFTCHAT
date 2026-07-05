7b9fb3c9028b161af3873b40a6deb49d86bd87aa
vicky kumar001 <vickykumar23499@gmail.com>
2026-07-05 17:34:01 +0530

    fix asgi import order

--- swiftchat/asgi.py
+++ swiftchat/asgi.py
@@ -6,21 +6,20 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-
 import os
-
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from chatapp import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swiftchat.settings')
 
+from django.core.asgi import get_asgi_application
+
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from chatapp import routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket": URLRouter(
         routing.websocket_urlpatterns
     )
-}) 
+})
